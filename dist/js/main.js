bffc2c0805b02b2795d4aa7a291b6b509e9b0517
Pietu <pietukor@gmail.com>
2024-07-17 02:01:10 +0300

    Layout 0.1.1: functional, responsive navigation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,20 @@
 //Import THREE.js
 import * as THREE from 'three';
 
+const showbar = document.getElementById("showBar");
+const hidebar = document.getElementById("hideBar");
+
 //open sidebar menu
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display='flex'
+
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
 }
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+} 
+
+
 
 
