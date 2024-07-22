efcfe13f46f73556c345722f39f6238728d44d7e
Pietu <pietukor@gmail.com>
2024-07-22 03:14:17 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const showbar = document.getElementById("showBar");
+const hidebar = document.getElementById("hideBar");
+
+//open sidebar menu
+
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+} 
+
+
+
+
